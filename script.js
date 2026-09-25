54803f368083529484eb3d0345dcdae056a727eb
isaias <isaias.villa123456@gmail.com>
2026-09-25 10:00:25 -0400

    created lab5.1 repo

---
+++ script.js
@@ -0,0 +1,44 @@
+const productNameInput = document.getElementById('product-name');
+const productPriceInput = document.getElementById('product-price');
+const addProductButton = document.getElementById('add-product');
+const cart = document.getElementById('cart');
+const totalPriceSpan = document.getElementById('total-price');
+ 
+let totalPrice = 0;
+let shoppingCart = [];
+ 
+
+addProductButton.addEventListener("click", ()=> {
+    let item = productNameInput.value;
+    let price = productPriceInput.value;
+    let product = {
+        item,price
+    }
+if (item === "" || price === "") {
+    alert("Please enter a product or price!");
+    return;
+  }
+    let cartList = document.createElement("li");
+    cartList.innerText = product
+
+    shoppingCart.push(product)
+    console.log(shoppingCart);
+    // updateTotalPrice(price)
+
+
+    item.value = "";
+    price.value = "";
+});
+// Function to update the total price
+function updateTotalPrice(amount) {
+  totalPrice += amount;
+  totalPriceSpan.textContent = totalPrice.toFixed(2);
+}
+ 
+// Function to remove an item
+function removeItem(event) {
+  const item = event.target.closest('li');
+  const price = parseFloat(item.dataset.price);
+  updateTotalPrice(-price);
+  item.remove();
+}
